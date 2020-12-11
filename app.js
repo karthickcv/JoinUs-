a709966481f274b53003fb2056188e2daa585309
karthickcv <karthickcv10423@gmail.com>
2020-12-11 17:41:42 +0530

    Table name changed

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended :true}));
 
 	app.get("/",function(req,res)
 		{
-			var q = 'SELECT COUNT(*) AS count FROM users';
+			var q = 'SELECT COUNT(*) AS count FROM users_tb';
 		 connection.query(q,function(err,results){
 			 if (err) throw err;
 			 var count = results[0].count;
